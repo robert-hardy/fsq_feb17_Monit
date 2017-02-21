28732d630781a3dfbba063a827088f25b83ba9a5
Robert Hardy <robbert@gmail.com>
2017-02-21 19:36:54 +0000

    Start as a bg process and write the pid.

--- services/app1/controller.sh
+++ services/app1/controller.sh
@@ -2,7 +2,8 @@ CMD=$1
 
 case $CMD in
     start)
-        /home/robert/k/venv/bin/python services/app1/run.py
+        /home/robert/k/venv/bin/python /home/robert/k/services/app1/run.py &
+        echo $! > /home/robert/k/pidfiles/app1.pid
         ;;
     stop)
         echo Stopping
