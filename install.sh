1a644a5a4af8fdb281bd19616b0e56ced4546b22
Robert Hardy <robbert@gmail.com>
2017-02-21 20:12:06 +0000

    Update install to put nginx config.

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ sudo cp monit/monitrc /etc/monit/
 
 sudo cp /home/robert/k/services/app1/app1 /etc/monit/conf.d/
 sudo cp /home/robert/k/services/app2/app2 /etc/monit/conf.d/
+
+
+sudo cp /home/robert/k/nginx/default /etc/nginx/sites-available/
